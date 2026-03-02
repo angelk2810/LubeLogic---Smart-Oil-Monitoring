ce104b884798560547c6630538c8f106ad0390c1
Angel K <angelkp1102@gmail.com>
2026-03-02 12:00:47 +0530

    Fix base path in vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/LubeLogic---Smart-Oil-Monitoring
-/',
+  base: '/LubeLogic---Smart-Oil-Monitoring/',
   plugins: [react()],
 });
