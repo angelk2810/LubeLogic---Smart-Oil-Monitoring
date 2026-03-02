a07813629178084fcbd891753cda84586b716504
Angel K <angelkp1102@gmail.com>
2026-03-02 12:18:44 +0530

    Integrate Tailwind CSS into Vite config
    Added Tailwind CSS plugin to Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/LubeLogic---Smart-Oil-Monitoring/',
-  plugins: [react()],
-});
+  base: '/LubeLogic---Smart-Oil-Monitoring/', // your repo name
+  plugins: [
+    react(),
+    tailwindcss(),   // ⚠ THIS IS REQUIRED
+  ],
+})
